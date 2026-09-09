b2aadf2772dd40a2f28b88d7fbbc28191f259efe
MrDwivedi25 <mrdwivedi74@gmail.com>
2026-09-09 21:29:15 +0530

    first commit

---
+++ leet code question 2/Question 3/counter.js
@@ -0,0 +1,18 @@
+let count = 0;
+ 
+
+function increment() {
+    count++;
+    return count;
+}
+
+function decrement() {
+    count--;
+    return count;
+
+}
+
+function reset() {
+    count = 0;
+    return count;
+}
